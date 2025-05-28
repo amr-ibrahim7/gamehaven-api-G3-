d6a3a3b0e0c1c99f49a1cebe396e2d192d729d34
Mohmed <mezoxmoonx@gmail.com>
2025-05-28 22:44:40 +0300

    add models and game schema

---
+++ src/models/userModel.js
@@ -0,0 +1,14 @@
+/*
+import mongoose and bcrypt
+userSchema
+name:  string requried
+email : string required  unique
+password: string required 
+role: user or admin  by default admin 
+
+hashing the password before sending to the database
+
+
+
+
+*/
