cd343e50bbc4631ecb0789a33f2118acf1c7f14c
amr-ibrahim7 <amrnew100@gmail.com>
2025-06-05 09:42:46 +0300

    Setup MongoDB connection & .env config

---
+++ src/DB/dbConnection.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+    );
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
-console.log('hello world');
+import dotenv from 'dotenv';
+import express from 'express';
+import connectToDB from './DB/dbConnection.js';
+
+dotenv.config();
+
+const app = express();
+
+
+connectToDB();
+
+app.get('/', (req, res) => {
+  res.send('backend is working!');
+});
+
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
